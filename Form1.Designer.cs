c5550d85c9de9155914ea11d0e1c746a8853e725
sonlatui <sonlatui@yahoo.com>
2011-10-19 21:39:50 +0700

    met het suc

---
+++ Form1.Designer.cs
@@ -0,0 +1,236 @@
+﻿namespace WS_Host
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.status = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Base address";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(91, 16);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(283, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "http://localhost:8000/";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(17, 57);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(76, 17);
+            this.checkBox1.TabIndex = 2;
+            this.checkBox1.Text = "Show Mex";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(17, 90);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Endpoints";
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(69, 117);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(72, 17);
+            this.checkBox2.TabIndex = 4;
+            this.checkBox2.Text = "BasicHttp";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(69, 142);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(62, 17);
+            this.checkBox3.TabIndex = 5;
+            this.checkBox3.Text = "NetTcp";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            this.checkBox3.CheckedChanged += new System.EventHandler(this.checkBox3_CheckedChanged);
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Location = new System.Drawing.Point(69, 172);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(62, 17);
+            this.checkBox4.TabIndex = 6;
+            this.checkBox4.Text = "WsHttp";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            this.checkBox4.CheckedChanged += new System.EventHandler(this.checkBox4_CheckedChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(69, 214);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "&Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(188, 213);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Sto&p";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(304, 213);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "Save &Config";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(23, 259);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Status";
+            // 
+            // status
+            // 
+            this.status.AutoSize = true;
+            this.status.Location = new System.Drawing.Point(94, 259);
+            this.status.Name = "status";
+            this.status.Size = new System.Drawing.Size(88, 13);
+            this.status.TabIndex = 11;
+            this.status.Text = "                           ";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(147, 113);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(232, 20);
+            this.textBox2.TabIndex = 12;
+            this.textBox2.Text = "http://localhost:8000/Lib";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(147, 139);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(232, 20);
+            this.textBox3.TabIndex = 13;
+            this.textBox3.Text = "net.tcp://localhost:8080/Lib";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(147, 169);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(232, 20);
+            this.textBox4.TabIndex = 14;
+            this.textBox4.Text = "http://localhost:80/Lib";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(391, 314);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.status);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.checkBox4);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label status;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+
+namespace WS_Host
+{
+    public partial class Form1 : Form
+    {
+        ServiceHost httpHost;
+        ServiceHost netHost;
+        ServiceHost wsHost;
+
+        [ServiceContract]
+        public interface ILib
+        {
+            [OperationContract]
+            string GetTacGia(string TacPham);
+            
+        }
+        public class Lib : ILib
+        {
+            public string GetTacGia(string TacPham)
+            {
+                if (TacPham == "Nhat Ky Trong Tu")
+
+                    return "Ho Chi Minh";
+                if (TacPham == "So Do")
+
+                    return "Vu Trong Phung";
+                if (TacPham == "Tat Den")
+
+                    return "Ngo Tat To";
+                return "Khong Biet =.=!";
+
+            }
+        }   
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox3_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox4_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            status.Text = "";
+            Type contractType = typeof(ILib);
+            Type instanceType = typeof(Lib);
+            if (checkBox2.Checked)
+            {
+                httpHost = new ServiceHost(instanceType, new Uri[] { new Uri(textBox1.Text) });
+                try
+                {
+                    httpHost.AddServiceEndpoint(contractType, new BasicHttpBinding(), textBox2.Text);
+                    status.Text += "BasicHTTP Ready\n";
+                    //System.Diagnostics.Process.Start(textBox1.Text);
+                    if (checkBox1.Checked)
+                    {
+                        ServiceMetadataBehavior behavior = new ServiceMetadataBehavior();
+                        behavior.HttpGetEnabled = true;
+                        httpHost.Description.Behaviors.Add(behavior);
+                        httpHost.AddServiceEndpoint(typeof(IMetadataExchange), new BasicHttpBinding(), "MEX");
+                    }
+                    httpHost.Open();
+                }
+                catch (Exception ex)
+                {
+                    status.Text = ex.Message;
+                    httpHost.Abort();
+                }
+            }
+            if (checkBox3.Checked)
+            {
+                
+                try
+                {
+                    NetTcpBinding A = new NetTcpBinding(SecurityMode.Transport);
+                    netHost = new ServiceHost(instanceType, new Uri[] { new Uri(textBox3.Text) });
+                    netHost.AddServiceEndpoint(contractType, A, textBox3.Text);
+                    netHost.Open();
+                    status.Text += "NetTcp Ready\n";
+                    //System.Diagnostics.Process.Start(textBox3.Text); 
+                }
+                catch (Exception ex)
+                {
+                    status.Text = ex.Message;
+                    netHost.Abort();
+                }
+            }
+            if (checkBox4.Checked)
+            {
+                wsHost = new ServiceHost(instanceType, new Uri[] { new Uri(textBox4.Text) });
+                try
+                {
+                    wsHost.AddServiceEndpoint(contractType, new WSHttpBinding(), textBox4.Text);
+                    if (checkBox1.Checked)
+                    {
+                        ServiceMetadataBehavior behavior = new ServiceMetadataBehavior();
+                        behavior.HttpGetEnabled = true;
+                        wsHost.Description.Behaviors.Add(behavior);
+                        wsHost.AddServiceEndpoint(typeof(IMetadataExchange), new WSHttpBinding(), "MEX");
+                    }
+                    wsHost.Open();
+                    status.Text += "WsHTTP Ready";
+                    //System.Diagnostics.Process.Start(textBox4.Text);
+                }
+                catch (Exception ex)
+                {
+                    status.Text = ex.Message;
+                    wsHost.Abort();
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (checkBox2.Checked)
+            {
+                httpHost.Abort();
+            }
+            if (checkBox3.Checked)
+            {
+                netHost.Abort();
+            }
+            if (checkBox4.Checked)
+            {
+                wsHost.Abort();
+            }
+        }
+    }
+}
